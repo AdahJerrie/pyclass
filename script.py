b17f116fc7297d27679072bc51c79f36c71e17f5
AdahJerrie <adahjerry51@gmail.com>
2026-07-29 06:23:56 +0100

    first py commit

---
+++ script.py
@@ -0,0 +1,17 @@
+#variables
+
+name = "Jerrie" 
+user_id = 324354545
+progress = 0.55 
+xp = 266
+average_score = 90.7
+verified = True 
+
+xp = 889 
+
+print(xp)
+
+#Datatypes - 
+print(type(name))
+print(type(progress))
+print(type(verified))
