e957b34e3b655c0613b167a3958cc330516bf6da
AdahJerrie <adahjerry51@gmail.com>
2026-09-05 20:33:59 +0100

    py basics

--- script.py
+++ script.py
@@ -1,17 +1,37 @@
 #variables
+# Python is object-oriented and dynamically typed.
+
+# When Python encounters:
+
+# name = "Jerrie"
+
+# it creates/uses a string object containing "Jerrie" and makes the name name refer to that object.
+
+# Conceptually:
+
+# name
+#   │
+#   ▼
+# "Jerrie"
+# Python variables are names that refer to objects.
 
 name = "Jerrie" 
 user_id = 324354545
-progress = 0.55 
 xp = 266
-average_score = 90.7
-verified = True 
+average_score = 70.7
+is_developer = True  
 
-xp = 889 
+if average_score >= 75:
+    print("passed")
+else:
+    print("Try again")
 
-print(xp)
-
-#Datatypes - 
+# #Datatypes - PYTHON figures out the type from the value. that makes python dynamically typed.
 print(type(name))
-print(type(progress))
-print(type(verified))
+print(type(user_id))
+print(type(is_developer))
+print(f"my name is {name} I have a score that is higher than {average_score}")
+# the f in the print value indicate format string.
+
+print(f"the sum is {average_score + xp}")
+print(f"the sum is {average_score // xp}")
