2a929b250d7cd248deb6f367de41fb9324e298e3
AdahJerrie <adahjerry51@gmail.com>
2026-09-06 14:24:46 +0100

    .

--- py-projects/cli_calc.py
+++ py-projects/cli_calc.py
@@ -1,8 +1,17 @@
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
 
+
+
 print(f"Addition: {first_number + second_number}")
 print(f"Subtraction: {first_number - second_number}")
 print(f"Multiplication: {first_number * second_number}")
-print(f"Division: {first_number / second_number}")
-print(f"Remainder: {first_number % second_number}")
+# print(f"Division: {first_number / second_number}")
+
+
+if second_number == 0:
+    print("Division: Cannot divide by zero.")
+    print("Remainder: Cannot calculate remainder.")
+else:
+    print(f"Division: {first_number / second_number}")
+    print(f"Remainder: {first_number % second_number}")
