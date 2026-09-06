281b69717761e66f56920ba15a457e8a75207140
AdahJerrie <adahjerry51@gmail.com>
2026-09-06 12:20:58 +0100

    .

--- script.py
+++ script.py
@@ -52,5 +52,3 @@ age = int(input("How old are you? "))
 print(f"Hello, {name}!")
 print(f"You are {age} years old.")
 print(f"Next year you will be {age + 1}.")
-
-# NB: Python often favours concise, expressive constructs.
