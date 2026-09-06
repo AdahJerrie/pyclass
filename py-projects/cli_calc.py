3a3423135045a2d61b42a7e8d0c4d31ba646219d
AdahJerrie <adahjerry51@gmail.com>
2026-09-06 10:39:29 +0100

    basic projects

---
+++ py-projects/cli_calc.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+print(f"Addition: {first_number + second_number}")
+print(f"Subtraction: {first_number - second_number}")
+print(f"Multiplication: {first_number * second_number}")
+float(print(f"Division: {first_number / second_number}"))
+print(f"Remainder: {first_number % second_number}")
---
+++ py-projects/introduction.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: " )
+age = input("How old are you? ")
+country = input("what country do you reside? ")
+language = input("what is your favourite programming language? ")
+
+print(f"Hello {name}, you are {age} years old and you live in {country}, finally your favorite programming language is {language}")
