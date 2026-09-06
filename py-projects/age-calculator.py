921eb3fb44c0fcc4a8a183bcca832e56b7bd8953
AdahJerrie <adahjerry51@gmail.com>
2026-09-06 10:46:28 +0100

    added age calc

---
+++ py-projects/age-calculator.py
@@ -0,0 +1,5 @@
+age = int(input("what is your current age? "))
+
+print(f"You will be {age}+1 years next year")
+print(f"You will be {age}+5 years in 5 years")
+print(f"You will be {age}+10 years in 10 years")
