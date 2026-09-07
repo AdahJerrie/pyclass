c2d9257fdba5dacb3731dabb42198d1aa681efc3
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-07 15:02:00 +0100

    exercises day2

---
+++ py-projects/evenodd.py
@@ -0,0 +1,18 @@
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+# Exercise 2: Positive, Negative or zero
+
+number2 = int(input("Enter a number: "))
+
+if number2 > 0:
+    print("Number is positive")
+elif number2 == 0:
+    print("Number is zero")
+else:
+    print("Number is negative")
+
