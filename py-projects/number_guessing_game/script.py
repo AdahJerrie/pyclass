510d4fb3f94481352e1140f6ef8672ad233653fc
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-07 15:36:30 +0100

    .

--- py-projects/number_guessing_game/script.py
+++ py-projects/number_guessing_game/script.py
@@ -1,17 +1,11 @@
-import random
-
-secret_number = random.randint(1, 100)
-
-print("I am thinking of a number between 1 and 100.") 
+secret_number = 7
 
 while True:
     guess = int(input("Guess the number: "))
     if guess < secret_number:
-        print("Too low!")
-        print("Try again")
+        print("Too low! Try again")
     elif guess > secret_number:
-        print("Too high!")
-        print("Try again")
+        print("Too high! Try again")
     else:
-        print("correct!💯")
+        print("Correct!💯")
         break
