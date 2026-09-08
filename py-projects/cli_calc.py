6e33270bfbd0e23e95af46b920901adf23718e67
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-08 11:23:20 +0100

    calc updated

--- py-projects/cli_calc.py
+++ py-projects/cli_calc.py
@@ -11,15 +11,21 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-    return a * b
+    return a * b 
 
 def divide(a, b):
     if b == 0:
         return ("Division: Cannot divide by zero.")
     else:
-        return (f"Division: {a / b}")
+        return a / b
     
-sum = add(a, b)
-print("Addition: {sum}")
+add_result = add(a, b)
+print(f"Addition: {add_result}")
+subtract_result = subtract(a, b)
+print(f"Subtraction: {subtract_result}")
+multiply_result = multiply(a, b)
+print(f"Multiplication: {multiply_result}")
+Divide_result = divide(a, b)
+print(f"Division: {Divide_result}")
 
     
