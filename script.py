7460dee5bb48bf8ebabf72a807092f1066f5af27
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-09 16:50:18 +0100

    list breakdown

--- script.py
+++ script.py
@@ -140,3 +140,14 @@ def check_number(number):
         return "Positive"
 
     return "Not positive"
+
+# week1 day4
+# 1. Lists — your most commonly used collection
+
+# A list stores multiple values in a particular order.
+
+# languages = ["Python", "Go", "JavaScript"]
+
+# You can access individual items using an index.
+
+# print(languages[0])
