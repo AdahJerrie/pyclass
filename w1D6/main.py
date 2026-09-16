1f40eb7ef4b145cb077888a509995b38ffc25674
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-16 17:05:10 +0100

    direct import and from import types

---
+++ w1D6/main.py
@@ -0,0 +1,5 @@
+import calculator 
+
+result = calculator.add(2, 4)
+
+print(result)
---
+++ w1D6/script.py
@@ -0,0 +1,9 @@
+# Day 6 will move into modules, imports, and organizing Python programs.
+# We'll also start thinking about how real Python projects are structured, rather than keeping everything in one file.
+# This is a transition from writing py code to organizing py code.
+# In python you split code into modules, in Go it is packages.
+# A module is essentially a way of packaging related Python code into one file.
+
+import math
+
+print(math.sqrt(25))
