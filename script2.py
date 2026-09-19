11d9ea52425cfde7d9f4882a41868984e8195e43
AdahJerrie <adahjerry51@gmail.com>
2026-09-19 23:18:01 +0100

    multiple checker

--- script2.py
+++ script2.py
@@ -29,3 +29,13 @@ if is_even(num_check):
     print(f"{num_check} is even")
 else:
     print(f"{num_check} is not even")
+
+# 2. write a python program to check if a number is a multiple of 3, a multiple of 5 or both.
+x = eval(input('enter a number      '))
+if x % 3 == 0 and x % 5 == 0:
+        print(f"this a multiple of 3 and 5: ", x)
+elif x % 3 == 0:
+    print(f"this is a multiple of 3: ", x)
+
+elif x % 5 == 0:
+    print(f"this is a multiple of 5: ", x) 
