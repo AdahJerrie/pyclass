91c8413fc1b166fe8ce1379b44e592eb09f993e7
AdahJerrie <adahjerry51@gmail.com>
2026-09-19 23:14:25 +0100

    feat: even checker

--- script2.py
+++ script2.py
@@ -18,3 +18,14 @@ def is_numeric(val):
 
     squares_dict = {num:num**2 for num in vald_numbers}
     print(squares_dict)
+
+# 1. write a python program to check if a number is even or odd
+def is_even(num):
+    if num % 2 == 0:
+        return num
+
+num_check = eval(input("enter a number\t: "))
+if is_even(num_check):
+    print(f"{num_check} is even")
+else:
+    print(f"{num_check} is not even")
