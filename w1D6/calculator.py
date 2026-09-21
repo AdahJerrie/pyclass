64d7c5e79f60e6dea941fd6d6d62738678b44e11
AdahJerrie <adahjerry51@gmail.com>
2026-09-21 15:10:34 +0100

    module creation

--- w1D6/calculator.py
+++ w1D6/calculator.py
@@ -3,3 +3,23 @@ def add(a,b):
 
 def subtract(a,b):
     return a - b
+
+def multiply(a,b):
+    return a * b
+
+if __name__ == "__main__":
+    add_result = add(2, 4)
+
+    print(f"addition: {add_result}")
+
+# import math
+
+# number = eval(input("enter a number\t: "))
+
+# sqrt_number = math.sqrt(number)
+# square_number = number ** 2
+
+# print(f"square root: {sqrt_number}")
+# print(f"power of 2: {square_number}")
+# print(f"pi: {math.pi}")
+
--- w1D6/main.py
+++ w1D6/main.py
@@ -1,5 +1,9 @@
 import calculator 
 
-result = calculator.add(2, 4)
+add_result = calculator.add(2, 4)
+subtract_result = calculator.subtract(4, 2)
+multiply_result = calculator.multiply(2, 4)
 
-print(result)
+print(f"addition: {add_result}")
+print(f"subtraction: {subtract_result}")
+print(f"multiplication: {multiply_result}")
