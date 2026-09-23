6f2d2932a5aea0a4b2de70b7e26571cae60b9afa
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-23 12:00:00 +0100

    truthiness simplified

--- wk1Recap/script.py
+++ wk1Recap/script.py
@@ -16,11 +16,11 @@ elif score > 60:
     print("B")
 else:
     print("pass")
-
+# --------------------------------------------------------------------------------------------------------------
 age = 20
-has_id = True 
+has_id = False 
 
-if age >= 18 and has_id:
+if age >= 18 and not has_id:
     print("access granted")
 else:
     print("access denied")
@@ -31,3 +31,52 @@ if day == "saturday" or day == "sunday":
     print("weekend")
 else:
     print("weekday")   
+# --------------------------------------------------------------------------------------------------------
+# Nested conditionals
+age >= 18
+has_cv = True
+qualified = age and has_cv
+
+if qualified:
+    print("access granted")
+else:
+    print("access denied")
+
+good_eyes = 2
+half_eyes = 1 
+
+employeable = good_eyes and not half_eyes
+
+if employeable:
+    print(employeable)
+else:
+    print(not employeable)
+
+# ---------------------------------------------------------------------------------------------
+
+is_student = False
+is_senior = True
+is_staff = False
+
+if is_student or is_senior or is_staff:
+    print("Discount available")
+
+# -----------------------------------------------------------------------------------------------------------
+# Nested conditionals
+age = 30
+has_car = True
+can_walk = True
+
+if age <= 30:
+    if has_car:
+        if can_walk:
+            print("can travel with us")
+    else:
+        print("you must have a car")
+else:
+    print("you are too old")
+# ------------------------------------------------------------------------------------------------
+name = input("enter your name: ")
+
+if name:
+    print(f"welcome {name}!")
