ea1fb843a535effbdd92dfb9b4cb9cc87b3dfb0d
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-23 11:07:38 +0100

    if/else statements and and

--- wk1Recap/script.py
+++ wk1Recap/script.py
@@ -7,3 +7,20 @@ if temp > 35:
 
 else:
     print("normal weather")
+
+score = 50
+
+if score >= 70:
+    print("A")
+elif score > 60:
+    print("B")
+else:
+    print("pass")
+
+age = 20
+has_id = True 
+
+if age >= 18 and has_id:
+    print("access granted")
+else:
+    print("access denied")
