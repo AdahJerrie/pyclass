ef55349eb46f26ebafa8333021651e7e3793625f
AdahJerrie <adahjeremiah3@gmail.com>
2026-09-25 14:25:57 +0100

    collections challenge

--- wk1Recap/task1.py
+++ wk1Recap/task1.py
@@ -36,20 +36,45 @@ three_check = 3 in unique_numbers
 # ---------------------------------------------------------------------------------------------------------------------------------
 # dictionaries
 # Dictionaries 🗂️Create a dictionary representing a developer profile:developer = { "name": "Jerrie", "age": 25, "language": "Go", "is_learning_python": True}Your tasksPrint the developer's name.Print their age.Print their programming language.Change "language" from "Go" to "Python".Add a new key:"experience": 2Check whether "age" exists in the dictionary.Loop through the dictionary and print each key and value.ChallengeWithout running the code first, predict what this will produce:print(developer["country"])
-developer = {"name": "Jerrie", "age": "25", "language": "Go", "is_learning_python": True}
-print(developer["name"])
-print(developer["age"])
-print(developer["language"])
-developer["language"] = "python"
-print(developer["language"])
-developer["experience"] = 2
-print(developer["experience"])
-print(developer)
+developer = {
+    "name": "Jerrie", 
+    "age": "25", 
+    "language": "Go", 
+    "is_learning_python": True
+}
+# print(developer["name"])
+# print(developer["age"])
+# print(developer["language"])
+# developer["language"] = "python"
+# print(developer["language"])
+# developer["experience"] = 2
+# print(developer["experience"])
+# print(developer)
 
 
 two_check = "age" in developer
-print(two_check)
+# print(two_check)
 
-for key, value in developer.items():
-    print(f"{key}: {value}")
+# for key, value in developer.items():
+    # print(f"{key}: {value}")
+# -----------------------------------------------------------------------------------------------------------
+# Collections Challenge 🧠Now we're going to combine list + tuple + set + dictionary.Imagine you're building a small developer skills tracker.Starting datalanguages = ["Python", "Go", "Python", "JavaScript", "Go"]developer = { "name": "Jerrie", "experience": 2}coordinates = (6.5244, 3.3792)Your tasks1. ListPrint the languages.Then add "Rust" to the list.2. SetConvert languages into a set called unique_languages.Print it.Your goal is to remove the duplicate "Python" and "Go".3. TuplePrint the latitude and longitude separately using indexes.Remember:coordinates[0] → ?coordinates[1] → ?4. DictionaryAdd this information to developer:"languages": unique_languagesThen print the entire dictionary.
+languages = ["Python", "Go", "Python", "JavaScript", "Go"]
+developer = {
+     "name": "Jerrie", 
+     "experience": 2
+}
+coordinates = (6.5244, 3.3792)
+# print(languages)
+languages.append("Rust")
+print(languages)
 
+# convert to set
+unique_languages = set(languages)
+print(unique_languages)
+# tuple
+print(coordinates[0])
+print(coordinates[1])
+
+developer["languages"] = unique_languages
+print(developer)
